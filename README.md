df5faf85b11f24a9951ac41c14a86ed6b8f496c7
unknown <wesleyray95@gmail.com>
2025-12-25 16:40:29 +0100

    updated glsl shader to display sphere with two-sided texture

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# Marballers
 
+# Marballers
 Simple web game using Odin + Sokol.
 
 # Build (Windows)
-
 Install emscripten and Odin (Sokol is embedded)
 
 ## Local
@@ -14,7 +13,6 @@ Requires WSL
 `.\deploy_to_server.ps1`
 
 # Dependencies
-
 Odin
 Sokol
 https://github.com/karl-zylinski/odin-sokol-web
--- source/shader.glsl
+++ source/shader.glsl
@@ -17,16 +17,33 @@ out vec4 color;
 
 void main() {
     gl_Position = mvp * position;
-    if (draw_mode == 0.0) {
-        color = vec4((normal + 1.0) * 0.5, 1.0);
-    }
-    else if (draw_mode == 1.0) {
-        color = vec4(texcoord, 0.0, 1.0);
-    }
-    else {
-        color = color0;
-    }
+    // if (draw_mode == 0.0) {
+    //     color = vec4((normal + 1.0) * 0.5, 1.0);
+    // }
+    // else if (draw_mode == 1.0) {
+    //     color = vec4(texcoord, 0.0, 1.0);
+    // }
+    // else {
+    //     color = color0;
+    // }
+
+    vec3 N = normalize(normal);
+    //vec3 base = (N.x > 0.0) ? vec3(1.0, 0.2, 0.2) : vec3(0.2, 0.4, 1.0);
+
+    vec3 color1 = vec3(0.9, 0.9, 0.9);
+    vec3 color2 = vec3(0.2, 0.7, 0.2);
+
+    vec3 base = (N.x > 0.0) ? color1 : color2;
+
+    color = vec4(base, 1.0);
 }
+@end
+
+@block fs_params
+uniform fs_params {
+    vec3 light_dir;
+    float ambient;
+};
 @end
 
 @fs fs
